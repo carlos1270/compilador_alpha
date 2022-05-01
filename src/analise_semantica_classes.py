d94a3e6339eeab8e17134ac1c9c0efe907aa0ccc
KelwinJonas <kelwinjonas@gmail.com>
2022-05-01 15:01:38 -0300

    regras semanticas para retorno de funcao

--- src/analise_semantica_classes.py
+++ src/analise_semantica_classes.py
@@ -32,4 +32,39 @@ class VariavelHash:
         return nome in self.variaveis
 
     def size(self):
-        return len(self.variaveis)    
+        return len(self.variaveis)
+
+class Funcao:
+    INTEGER = 1
+    BOLEANO = 2
+    VAZIO = 3
+
+    def __init__(self, nome, tipo, parametros, lexval):
+        self.nome = nome
+        self.tipo = tipo
+        self.parametros = parametros
+        self.lexval = lexval
+
+    def print(self):
+        print("Variavel: " + str(self.nome) + ", Tipo: " + str(self.tipo) + ", Parametros: " + str(self.parametros) + ", Lexval: " + str(self.lexval))
+
+
+class FuncaoHash:
+
+    def __init__(self):
+        self.funcoes = {}
+    
+    def add(self, funcao):
+        self.funcoes[funcao.nome] = funcao
+
+    def print(self):
+        print("========================= SEMANTICA ===========================")
+        for i in self.funcoes:
+            self.funcoes[i].print()
+        print("========================= X SEMANTICA X ===========================")
+
+    def exists(self, nome):
+        return nome in self.funcoes
+
+    def size(self):
+        return len(self.funcoes)    
